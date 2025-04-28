18c2c55cff86c7abb350941b1e2c5014a0a36832
EyaCode <eya.filali@esprit.tn>
2025-04-28 15:25:15 +0100

    first commit

---
+++ src/functions/index_creation.py
@@ -0,0 +1,131 @@
+import boto3
+from crhelper import CfnResource
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+
+helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120, ssl_verify=None)
+
+
+# No-op for update and delete
+@helper.update
+@helper.delete
+def no_op(event, context):
+    pass
+
+
+# get aoss_host from os environ
+def get_aoss_host(resource_properties):
+    """
+    Extract and sanitize the AOSS host from resource properties.
+    Example: 'abc123.region.aoss.amazonaws.com'
+    """
+    if "AOSSHost" not in resource_properties:
+        raise Exception("AOSSHost not provided in resource properties")
+
+    raw_host = resource_properties["AOSSHost"]
+    print("loooook  here eya"+raw_host)
+    # Remove any accidental protocol or slashes
+    sanitized_host = raw_host.replace("https://", "").split("/")[0]
+    print("loooook also her"+sanitized_host)
+    return sanitized_host
+
+
+def get_aoss_client(host):
+    auth = AWSV4SignerAuth(
+        boto3.Session().get_credentials(),
+        boto3.session.Session().region_name,
+        "aoss"
+    )
+    # create an opensearch client and use the request-signer
+    return OpenSearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+
+
+def get_aoss_index_name(resource_properties):
+    if "AOSSIndexName" not in resource_properties:
+        raise Exception("AOSSIndexName not provided from resource properties")
+    return resource_properties["AOSSIndexName"]
+
+
+# Function to use the opensearch-py library to create an index within an opensearch collection
+def create_aoss_index(index_name, aos_client):
+    index_body = {
+    "settings" : {
+        "index":{
+            "number_of_shards" : 1,
+            "number_of_replicas" : 0,
+            "knn": "true",
+            "knn.algo_param.ef_search": 32
+        }
+    },
+        "mappings": {
+            "dynamic_templates": [
+                {
+                    "strings": {
+                        "match_mapping_type": "string",
+                        "mapping": {
+                            "fields": {
+                                "keyword": {
+                                    "ignore_above": 2147483647,
+                                    "type": "keyword"
+                                }
+                            },
+                            "type": "text"
+                        }
+                    }
+                }
+            ],
+            "properties": {
+                "AMAZON_BEDROCK_METADATA": {
+                    "type": "text"
+
+                },
+                "AMAZON_BEDROCK_TEXT_CHUNK": {
+                    "type": "text"
+                },
+                "bedrock-knowledge-base-default-vector": {
+                    "type": "knn_vector",
+                    "dimension": 1024,
+                    "method": {
+                        "engine": "faiss",
+                        "space_type": "l2",
+                        "name": "hnsw",
+                        "parameters": {}   #add k = 5
+
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    aos_client.indices.create(index=index_name, body=index_body)
+    print(f"Created index {index_name}")
+
+
+
+
+
+
+
+
+
+
+# Handles create event of the CloudFormation resource
+@helper.create
+def create_index(event, context):
+    resource_properties = event['ResourceProperties']
+    aoss_host = get_aoss_host(resource_properties)
+    aos_client = get_aoss_client(aoss_host)
+    index_name = get_aoss_index_name(resource_properties)
+    create_aoss_index(index_name=index_name, aos_client=aos_client)
+
+
+def lambda_handler(event, context):
+    print(event)
+    helper(event, context)
