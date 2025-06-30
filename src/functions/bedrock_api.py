727001daafd5e6d5ea493d011548ec59fa65bb14
Ahmed Zeddini <ahmedzeddini@wattnow.io>
2025-06-30 12:10:21 +0000

    bedrock_api.py edited online with Bitbucket

--- src/functions/bedrock_api.py
+++ src/functions/bedrock_api.py
@@ -18,7 +18,7 @@ class AgentInvoker:
         session_id = str(uuid.uuid4())
         completion = ""
         citations = []
-
+        print("")
         try:
             logger.info(f"Invoking agent {self.agent_id} with alias {self.agent_alias_id}")
             response = self.agents_runtime_client.invoke_agent(
